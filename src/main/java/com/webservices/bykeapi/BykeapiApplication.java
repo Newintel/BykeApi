64e3913ee779e53fcd26a1fa64b2f605f165462c
Newintel <franck.labracherie@gmail.com>
2022-10-12 11:49:31 +0200

    first commit

---
+++ src/main/java/com/webservices/bykeapi/BykeapiApplication.java
@@ -0,0 +1,13 @@
+package com.webservices.bykeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BykeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BykeapiApplication.class, args);
+    }
+
+}
