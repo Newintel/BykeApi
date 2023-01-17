6b8d0ebe1febf170696682293fcd6118e3a3295c
Newintel <franck.labracherie@gmail.com>
2023-01-17 02:09:21 +0100

    remplacer le inner join des paths les plus populaires par un left join pour avoir tous les paths

--- src/main/java/com/webservices/bykeapi/repository/PathRepository.java
+++ src/main/java/com/webservices/bykeapi/repository/PathRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PathRepository extends JpaRepository<Path, Integer> {
-    @Query("select p from Path p inner join Excursion e on p.id = e.path.id group by p.id order by count(e.id.departure) desc")
+    @Query("select p from Path p left join Excursion e on p.id = e.path.id group by p.id order by count(e.id.departure) desc")
     List<Path> getMostPopular();
 
 
